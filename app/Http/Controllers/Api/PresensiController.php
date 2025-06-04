db8e1f19664a4bf90a2a864eab805aba0f7d9e34
se7entyx <christopherafael77@gmail.com>
2025-06-04 15:45:41 +0700

    helow

--- app/Http/Controllers/Api/PresensiController.php
+++ app/Http/Controllers/Api/PresensiController.php
@@ -182,7 +182,7 @@ class PresensiController extends Controller
         $cloudinaryPath = $user->username;
 
         // Kirim ke Flask API
-        $response = Http::post("https://5319-139-228-86-115.ngrok-free.app/verify", [
+        $response = Http::post("https://bf70-139-228-86-115.ngrok-free.app/verify", [
             'image' => $base64Image,
             'dataset_path' => $cloudinaryPath,
         ]);
