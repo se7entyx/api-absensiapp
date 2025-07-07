ae08aff92592dadb76a7ec9968448b1dcdff68c3
se7entyx <christopherafael77@gmail.com>
2025-07-07 15:19:19 +0700

    yrr

--- resources/views/presensi-exports-jam-kerja.blade.php
+++ resources/views/presensi-exports-jam-kerja.blade.php
@@ -4,31 +4,35 @@
             <th>Nama</th>
             <th>NIP</th>
             <th>Department</th>
+            <th>Jumlah Kehadiran</th>
             <th>Total Jam Kerja</th>
         </tr>
     </thead>
     <tbody>
         @foreach($users as $user)
-            @php
-                $totalSeconds = 0;
+        @php
+        $totalSeconds = 0;
+        $kehadiran = 0;
+        foreach ($user->presensi as $presensi) {
+            if ($presensi->check_out) {
+                $checkIn = \Carbon\Carbon::parse($presensi->created_at);
+                $checkOut = \Carbon\Carbon::parse($presensi->check_out);
+                $totalSeconds += $checkOut->diffInSeconds($checkIn);
+                $kehadiran++;
+            }
+        }
 
-                foreach ($user->presensi as $presensi) {
-                    if ($presensi->check_out) {
-                        $checkIn = \Carbon\Carbon::parse($presensi->created_at);
-                        $checkOut = \Carbon\Carbon::parse($presensi->check_out);
-                        $totalSeconds += $checkOut->diffInSeconds($checkIn);
-                    }
-                }
-
-                $totalHours = floor($totalSeconds / 3600);
-                $remainingMinutes = floor(($totalSeconds % 3600) / 60);
-            @endphp
-            <tr>
-                <td>{{ $user->name }}</td>
-                <td>{{ $user->uid }}</td>
-                <td>{{ $user->department->name ?? '-' }}</td>
-                <td>{{ $totalHours }} jam, {{ $remainingMinutes }} menit</td>
-            </tr>
+        $totalSeconds = abs($totalSeconds);
+        $totalHours = floor($totalSeconds / 3600);
+        $remainingMinutes = floor(($totalSeconds % 3600) / 60);
+        @endphp
+        <tr>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->uid }}</td>
+            <td>{{ $user->department->name ?? '-' }}</td>
+            <td>{{ $kehadiran }}</td>
+            <td>{{ $totalHours }} jam, {{ $remainingMinutes }} menit</td>
+        </tr>
         @endforeach
     </tbody>
 </table>
